95b58e3d9d372374c5f1ddff906e0deada555a2c
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-03-20 18:32:13 +0200

    Add variable for the  CIDR block for the VPC

--- variables.tf
+++ variables.tf
@@ -5,3 +5,9 @@ variable "aws_region" {
   type        = string
   default     = "eu-central-1"
 }
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
